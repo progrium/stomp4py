cfe2a5af6e3d53cdf601bfc2a9a1b67a053aff30
Jeff Lindsay <progrium@gmail.com>
2011-10-17 22:44:08 -0700

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='stomp4py',
+    version='0.1.0',
+    author='Jeff Lindsay',
+    author_email='jeff.lindsay@twilio.com',
+    description='',
+    packages=find_packages(),
+    data_files=[],
+)
